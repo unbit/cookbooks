e183960948ff9b84482398f0d58cc1b4f204a9ab
Roberto De Ioris <roberto@unbit.it>
2014-03-30 10:51:35 +0200

    fixed opsworks support

--- rails/recipes/assets.rb
+++ rails/recipes/assets.rb
@@ -1,5 +1,7 @@
-execute 'rake assets:precompile' do
-  cwd "#{node[:deploy_to]}/current"
-  user 'deploy'
-  command 'bundle exec rake assets:precompile'
+node[:deploy].each do |application, deploy|
+  execute 'rake assets:precompile' do
+    cwd "#{deploy[:deploy_to]}/current"
+    user 'deploy'
+    command 'bundle exec rake assets:precompile'
+  end
 end
