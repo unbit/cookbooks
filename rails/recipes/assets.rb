6ffcb0db5c9937695af3ef5e419d62257b3cbd50
Unbit <info@unbit.it>
2014-03-31 16:20:26 +0200

    try to fix assets precompilation

--- rails/recipes/assets.rb
+++ rails/recipes/assets.rb
@@ -1,7 +1,9 @@
 node[:deploy].each do |application, deploy|
+  rails_env = deploy[:rails_env]
   execute 'rake assets:precompile' do
     cwd "#{deploy[:deploy_to]}/current"
     user 'deploy'
     command 'bundle exec rake assets:precompile'
+    environment 'RAILS_ENV' => rails_env
   end
 end
