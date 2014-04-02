c97e0b9e1b39974e4f30e4ac67144efea8522556
Roberto De Ioris <info@unbit.it>
2014-04-03 00:06:56 +0200

    attempt automatic symlinks

--- rails/recipes/application.rb
+++ rails/recipes/application.rb
@@ -6,4 +6,6 @@ node[:deploy].each do |application, deploy|
     source "application.yml.erb"
     variables(:redis_url => deploy[:redis_url], :devise_key => deploy[:devise_key])
   end
+  current_path "/srv/www/rgts/current"
+  symlink_before_migrate {"config/application.yml"=>"config/application.yml"}
 end
